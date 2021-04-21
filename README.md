620c4058ce9581890d16aacc164f76a93bbb4971
langengel <langengel@qq.com>
2021-04-21 18:27:45 +0800

    init

--- README.md
+++ README.md
@@ -1,23 +1,26 @@
 # k8s-auto
 
 #### 介绍
-Kubernetes 自动安装部署脚本
+Kubernetes 自动安装部署脚本 一切都自动化都是为了懒惰都人类😄
 
 #### 软件架构
-软件架构说明
+Centos-7.6-x86_64-DVD-1810 + kubernetes1.19.4 + docker-ce-19.03.9 + calico
 
 
 #### 安装教程
 
-1.  xxxx
-2.  xxxx
-3.  xxxx
+1.  `yum install -y git`
+2.  `git clone https://gitee.com/langengel/k8s-auto.git`
+3.  `cd k8s-auto`
+4.  `chmod +x kuberneter-install.sh`
 
 #### 使用说明
 
-1.  xxxx
-2.  xxxx
-3.  xxxx
+1.  执行脚本 `./kuberneter-install.sh`
+2.  `set hostname default k8s-master`:设置主机名 默认为 k8s-master
+3.  `this hostname is k8s-master [y/n]？` 确定要设置就输入y 其他操作退出安装程序
+4.  `this node is master-node? [y/n] default no？` 输入y设置为主节点 输入其他都视为非主节点 非主节点将不初始化kubeadm
+4.  `set kube-api IP default 192.168.3.200` 输入kubeadm api IP 相当于设置--apiserver-advertise-address参数值 主节点必须设置否则退出安装脚本
 
 #### 参与贡献
 
@@ -29,9 +32,4 @@ Kubernetes 自动安装部署脚本
 
 #### 特技
 
-1.  使用 Readme\_XXX.md 来支持不同的语言，例如 Readme\_en.md, Readme\_zh.md
-2.  Gitee 官方博客 [blog.gitee.com](https://blog.gitee.com)
-3.  你可以 [https://gitee.com/explore](https://gitee.com/explore) 这个地址来了解 Gitee 上的优秀开源项目
-4.  [GVP](https://gitee.com/gvp) 全称是 Gitee 最有价值开源项目，是综合评定出的优秀开源项目
-5.  Gitee 官方提供的使用手册 [https://gitee.com/help](https://gitee.com/help)
-6.  Gitee 封面人物是一档用来展示 Gitee 会员风采的栏目 [https://gitee.com/gitee-stars/](https://gitee.com/gitee-stars/)
+1.  你有更好的请提pr, 不提供技术支持
---
+++ kuberneter-install.sh
@@ -0,0 +1,312 @@
+#! /bin/bash
+# author: langengel@qq.com
+# date: 2021-04-20
+# des: Automatic deployment Kubernetes
+# environment: Centos-7.6-x86_64-DVD-1810 + kubernetes1.19.4 + docker-ce-19.03.9 + calico
+
+# ------------------------------------log--------------------------------------------------------------
+# success log
+log_success() {
+  echo -e "\e[32m $1\e[0m"
+}
+# info log
+log_info() {
+  echo -e "\e[33m $1\e[0m"
+}
+# error log
+log_error() {
+  echo -e "\e[31m $1\e[0m"
+  exit
+}
+log_success "Thank you use k8s auto install shell!"
+
+# ------------------------------------set hostname----------------------------------------------------
+# set hostname
+read -p "set hostname default k8s-master " HOST_NAME
+if test ! -n "$HOST_NAME"; then
+  HOST_NAME="k8s-master"
+fi
+
+read -p "this hostname is $HOST_NAME [y/n] " CONFIRM
+if test "$CONFIRM" != "y"; then
+  log_error "program exit..."
+fi
+read -p "this node is master-node? [y/n] default no " IS_MASTER
+if test "$IS_MASTER" = "y"; then
+  read -p "set kube-api IP default 192.168.3.200 " KUBE_API_IP
+  if test ! -n "$KUBE_API_IP"; then
+    KUBE_API_IP="192.168.3.200"
+  else
+    log_error "kube-api Cannot be empty！program exit ..."
+  fi
+fi
+log_info "setting hostname is $HOST_NAME"
+hostnamectl set-hostname $HOST_NAME
+if test $?; then
+  log_success "setting hostname ok ..."
+fi
+
+# ------------------------------------disable firewalld-----------------------------------------------
+log_info "disable firewalld ..."
+systemctl stop firewalld && systemctl disable firewalld
+if test $?; then
+  log_success "firewall disable ok..."
+else
+  log_error "firewall disable fail "
+fi
+
+# ------------------------------------off swap -------------------------------------------------------
+log_info "off swap ..."
+swapoff -a && sed -ri 's/.*swap.*/#&/' /etc/fstab
+if test $?; then
+  log_success "swap off ok ..."
+else
+  log_error "swap off fail"
+fi
+
+# ------------------------------------load modules ---------------------------------------------------
+log_info "Load the required kernel modules"
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+modprobe overlay
+if test $?; then
+  log_success "modprobe overlay ok ..."
+else
+  log_error "modprobe overlay fail"
+fi
+modprobe br_netfilter
+if test $?; then
+  log_success "modprobe br_netfilter ok ..."
+else
+  log_error "modprobe br_netfilter fail"
+fi
+
+# ------------------------------------load ipvs modules ----------------------------------------------
+log_info "add ipvs modules ..."
+cat >/etc/modules-load.d/ipvs.conf <<EOF
+# Load IPVS at boot
+ip_vs
+ip_vs_rr
+ip_vs_wrr
+ip_vs_sh
+nf_conntrack_ipv4
+EOF
+log_info "enable now systemd modules service ..."
+systemctl enable --now systemd-modules-load.service
+if test $?; then
+  lsmod | grep -e ip_vs -e nf_conntrack_ipv4
+  log_success "ipvs load ok..."
+else
+  log_error "ipvs load fail"
+fi
+
+# ------------------------------------install ipset ipvsadm -----------------------------------------
+log_info "install ipset ipvsadm ... "
+yum install -y ipset ipvsadm
+if test $?; then
+  log_success "ipset ipvsadm install ok..."
+else
+  log_error "ipset ipvsadm install fail"
+fi
+
+# ------------------------------------set sysctl allow iptables -------------------------------------
+log_info "set sysctl allow iptables forward, kubernetes-cri conf  ... "
+log_info "set iptalbes bridge call and forward..."
+cat <<EOF | sudo tee /etc/sysctl.d/kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+vm.swappiness                       = 0
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+echo -e "\n\n"
+sysctl --system
+if test $?; then
+  log_success "set sysctl allow iptables forward, kubernetes-cri conf ok"
+else
+  log_error "set sysctl allow iptables forward, kubernetes-cri conf fial"
+fi
+
+# ------------------------------------install yum tool ----------------------------------------------
+log_info "install yum-utils device-mapper-persistent-data lvm2... "
+yum install -y yum-utils device-mapper-persistent-data lvm2 vim net-tools bash-completion
+if test $?; then
+  log_success "install yum-utils device-mapper-persistent-data lvm2 ok "
+else
+  log_error "install yum-utils device-mapper-persistent-data lvm2 fail "
+fi
+
+# ------------------------------------install docker ce ---------------------------------------------
+log_info "--add-repo     https://download.docker.com/linux/centos/docker-ce.repo ..."
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+if test $?; then
+  log_success "--add-repo ok"
+else
+  log_success "--add-repo fail"
+fi
+
+log_info "install docker-ce-19.03.9 docker-cli-19.03.9 ..."
+yum install -y docker-ce-19.03.9 docker-cli-19.03.9
+if test $?; then
+  log_success "install docker-ce-19.03.9 docker-cli-19.03.9 ok"
+else
+  log_fail "install docker-ce-19.03.9 docker-cli-19.03.9 fail"
+fi
+
+systemctl enable docker
+if test $?; then
+  log_success "enable docker ok "
+else
+  log_fail "enable docker fail"
+fi
+systemctl start docker
+if test $?; then
+  log_success "start docker ok "
+else
+  log_fail "start docker fail"
+fi
+
+# ------------------------------------registry mirrors- ---------------------------------------------
+log_info "set docker registry mirrors..."
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{ 
+ "registry-mirrors": ["https://9qdic84f.mirror.aliyuncs.com"],
+ "exec-opts": ["native.cgroupdriver=systemd"]
+}
+EOF
+
+sudo systemctl daemon-reload
+if test $?; then
+  log_success "daemon-reload ok "
+else
+  log_fail "daemon-reload fail"
+fi
+
+sudo systemctl restart docker
+if test $?; then
+  log_success "restart docker ok "
+else
+  log_fail "restart docker fail"
+fi
+
+# ------------------------------------docker network iptables forward service -----------------------
+log_info "set docker network iptables forward service..."
+CONFIG_PATH=/lib/systemd/system/docker.service
+sed -i '/ExecStartPost=/d' $CONFIG_PATH
+sed -i '/ExecStart=/i ExecStartPost=/sbin/iptables -I FORWARD -s 0.0.0.0/0 -j ACCEPT' $CONFIG_PATH
+if test $?; then
+  log_success "set docker network iptables forward service ok "
+else
+  log_fail "set docker network iptables forward service fail"
+fi
+
+# ------------------------------------set kubernetes yum mirrors ------------------------------------
+log_info "set kubernetes yum mirrors..."
+cat <<EOF >/etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64/
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
+EOF
+
+# ------------------------------------install kubernetes --------------------------------------------
+log_info "install kubelet-1.19.4 kubeadm-1.19.4 kubectl-1.19.4..."
+yum install -y kubelet-1.19.4 kubeadm-1.19.4 kubectl-1.19.4
+if test $?; then
+  log_success "install kubelet-1.19.4 kubeadm-1.19.4 kubectl-1.19.4 ok "
+else
+  log_fail "install kubelet-1.19.4 kubeadm-1.19.4 kubectl-1.19.4 fail"
+fi
+systemctl enable kubelet
+if test $?; then
+  log_success "enable kubelet ok "
+else
+  log_fail "enable kubelet fail"
+fi
+systemctl enable kubelet
+if test $?; then
+  log_success "start kubelet ok "
+else
+  log_fail "start kubelet fail"
+fi
+
+# ------------------------------------time synchronization ------------------------------------------
+log_info "yum install ntpdate..."
+yum install ntpdate
+if test $?; then
+  log_success "install ntpdate ok "
+else
+  log_fail "install ntpdate fail"
+fi
+
+log_info "time synchronization..."
+ntpdate time.windows.com
+if test $?; then
+  log_success "time synchronization ok "
+else
+  log_fail "time synchronization fail"
+fi
+
+# ------------------------------------kubectl completion bash ---------------------------------------
+echo "source <(kubectl completion bash)" >>~/.bashrc
+source .bashrc
+if test $?; then
+  log_success "kubectl completion bash ok "
+else
+  log_fail "kubectl completion bash fail"
+fi
+
+# ------------------------------------kubeadm init --------------------------------------------------
+if test "$IS_MASTER" = "y"; then
+  log_info "create kubeadm-config.yaml..."
+  cat >kubeadm-config.yaml <<EOF
+apiVersion: kubeadm.k8s.io/v1beta2
+kind: InitConfiguration
+localAPIEndpoint:
+  advertiseAddress: ${KUBE_API_IP}
+  bindPort: 6443
+nodeRegistration:
+  taints:
+  - effect: PreferNoSchedule
+    key: node-role.kubernetes.io/master
+---
+apiVersion: kubeadm.k8s.io/v1beta2
+kind: ClusterConfiguration
+imageRepository: registry.aliyuncs.com/google_containers
+kubernetesVersion: v1.19.4
+networking:
+  podSubnet: 10.244.0.0/16
+  serviceSubnet: 10.96.0.0/12
+---
+apiVersion: kubeproxy.config.k8s.io/v1alpha1
+kind: KubeProxyConfiguration
+featureGates:
+  SupportIPVSProxyMode: true
+mode: ipvs
+EOF
+
+  log_info "kubeadm init..."
+  kubeadm init --config kubeadm-config.yaml >&1 | tee kubernetes-init.log
+  if test $?; then
+    log_success "kubeadm init ok "
+  else
+    log_fail "kubeadm init fail"
+  fi
+  mkdir -p $HOME/.kube
+  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+  sudo chown $(id -u):$(id -g) $HOME/.kube/config
+  # ------------------------------------kube-proxy-calico --------------------------------------------------
+  log_info "apply kube-proxy..."
+  kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+  if test $?; then
+    log_success "kube-proxy install ok "
+  else
+    log_fail "kube-proxy install fail"
+  fi
+  exit
+fi
+exit
